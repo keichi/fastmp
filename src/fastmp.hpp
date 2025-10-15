8d71bf3e9d3e000a6a16b674081c02ef265073fc
Keichi Takahashi <hello@keichi.dev>
2025-10-15 23:24:28 +0900

    Port to VE using VEDA

--- src/fastmp.hpp
+++ src/fastmp.hpp
@@ -1,6 +1,10 @@
 #include <cstddef>
 
+#include <veda_device.h>
+
+extern "C" {
 void sliding_window_dot_product_fft(const double *T, const double *Q, double *QT, size_t n, size_t m);
 void sliding_window_dot_product_naive(const double *T, const double *Q, double *QT, size_t n, size_t m);
 void compute_mean_std(const double *T, double *mu, double *sigma, size_t n, size_t m);
-void stomp(const double * T, double * P, size_t n, size_t m);
+void stomp(VEDAdeviceptr T, VEDAdeviceptr P, size_t n, size_t m);
+}
--- tests/test_fastmp.py
+++ tests/test_fastmp.py
@@ -4,37 +4,46 @@ import stumpy
 import pytest
 
 
+# @pytest.mark.parametrize("n,m", [(100, 10), (500, 20), (1000, 100)])
+# def test_sliding_window_dot_product(n, m):
+#     T = np.random.rand(n)
+#     Q = np.random.rand(m)
+# 
+#     QT = fastmp.sliding_window_dot_product(T, Q)
+# 
+#     # Compare to naive calculation
+#     for i in range(n - m + 1):
+#         assert np.isclose(T[i:i+m] @ Q, QT[i])
+# 
+#     # Compare to np.convolve
+#     assert np.allclose(np.convolve(T, Q[::-1], mode="valid"), QT)
+# 
+# 
+# @pytest.mark.parametrize("n,m", [(100, 10), (500, 20), (1000, 100)])
+# def test_compute_mean_std(n, m):
+#     T = np.random.rand(n)
+# 
+#     mu, sigma = fastmp.compute_mean_std(T, m)
+# 
+#     for i in range(n - m + 1):
+#         assert np.isclose(np.mean(T[i:i+m]), mu[i])
+#         assert np.isclose(np.std(T[i:i+m]), sigma[i])
+
 @pytest.mark.parametrize("n,m", [(100, 10), (500, 20), (1000, 100)])
-def test_sliding_window_dot_product(n, m):
-    T = np.random.rand(n)
-    Q = np.random.rand(m)
-
-    QT = fastmp.sliding_window_dot_product(T, Q)
-
-    # Compare to naive calculation
-    for i in range(n - m + 1):
-        assert np.isclose(T[i:i+m] @ Q, QT[i])
-
-    # Compare to np.convolve
-    assert np.allclose(np.convolve(T, Q[::-1], mode="valid"), QT)
-
-
-@pytest.mark.parametrize("n,m", [(100, 10), (500, 20), (1000, 100)])
-def test_compute_mean_std(n, m):
+def test_selfjoin(n, m):
     T = np.random.rand(n)
 
-    mu, sigma = fastmp.compute_mean_std(T, m)
-
-    for i in range(n - m + 1):
-        assert np.isclose(np.mean(T[i:i+m]), mu[i])
-        assert np.isclose(np.std(T[i:i+m]), sigma[i])
-
-
-@pytest.mark.parametrize("n,m", [(100, 10), (500, 20), (1000, 100)])
-def test_stomp(n, m):
-    T = np.random.rand(n)
-
-    mp = fastmp.stomp(T, m)
+    mp = fastmp.selfjoin(T, m)
     mp2 = stumpy.stump(T, m)[:, 0].astype(np.float64)
 
+    assert np.allclose(mp, mp2)
+
+@pytest.mark.parametrize("n,m", [(100, 10), (500, 20), (1000, 100)])
+def test_abjoin(n, m):
+    T1 = np.random.rand(n)
+    T2 = np.random.rand(n)
+
+    mp = fastmp.abjoin(T1, T2, m)
+
+    mp2 = stumpy.stump(T_A=T1, T_B=T2, m=m, ignore_trivial=False)[:, 0].astype(np.float64)
     assert np.allclose(mp, mp2)
